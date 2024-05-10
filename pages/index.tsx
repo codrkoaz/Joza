986788ba43fe191e5c39157454eaf8b4da195056
codrkoaz <codrkoazie@gmail.com>
2024-05-09 21:16:46 -0400

    fix minor bugs and add better icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,31 @@ import Head from 'next/head';
 import AboutMe from '../components/Main';
 import Layout from '../components/Layout/layout';
 
-
 const Home: NextPage = () => {
-	return (
-		<>
-			<Layout>
-				<Container>
-					<Head>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Joza Smith's homepage" />
-        <meta name="author" content="Joza Smith" />
-        <meta name="author" content="codrkoaz" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta property="og:site_name" content="Joza Smith" />
-        <meta name="og:title" content="Joza Smith" />
-        <meta property="og:type" content="website" />
-						<title>codrkoaz • home </title>
-					</Head>
-					<AboutMe />
-				</Container>
-			</Layout>
-		</>
-	);
+  return (
+    <>
+      <Layout>
+        <Container>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta name="description" content="Joza Smith's homepage" />
+            <meta name="author" content="Joza Smith" />
+            <meta name="author" content="codrkoaz" />
+            <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+            <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+            <meta property="og:site_name" content="Joza Smith" />
+            <meta name="og:title" content="Joza Smith" />
+            <meta property="og:type" content="website" />
+            <title>codrkoaz • home </title>
+          </Head>
+          <AboutMe />
+        </Container>
+      </Layout>
+    </>
+  );
 };
 
 export default Home;
